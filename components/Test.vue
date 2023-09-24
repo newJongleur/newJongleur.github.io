7b276353bf3049ef23a83e3ed0434b52b2a7249a
newJongleur <505755935@qq.com>
2023-09-24 17:19:58 +0800

    one

--- components/Test.vue
+++ components/Test.vue
@@ -1,3 +1,10 @@
 <template>
-    Test
+    <button @click="toggleDark()">
+        按钮
+    </button>
 </template>
+
+<script lang="ts" setup>
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/normalize.css'],
+  css: ['~/assets/normalize.css', 'element-plus/dist/index.css'],
   modules: [
     '@element-plus/nuxt',
     '@nuxt/content',
