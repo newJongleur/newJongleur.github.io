2fa29c09400b250fa82dcdcb8987ceb4372cdc2e
newJongleur <505755935@qq.com>
2023-09-24 13:53:17 +0800

    one

---
+++ app.vue
@@ -0,0 +1,10 @@
+<!-- <template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template> -->
+<template>
+  <el-button @click="ElMessage('hello')">button</el-button>
+  <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+  <LazyElButton type="warning">lazy button</LazyElButton>
+</template>
